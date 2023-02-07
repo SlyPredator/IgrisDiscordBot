1a909e824006192d1592543ad2b91c956c719019
SlyPredator <navneethmahadevan@gmail.com>
2023-02-07 16:49:49 +0530

    change to .env

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import os
 import platform
 import random
 import sys
+from dotenv import load_dotenv
 
 import aiosqlite
 import discord
@@ -20,6 +21,11 @@ from discord.ext import commands, tasks
 from discord.ext.commands import Bot, Context
 
 import exceptions
+
+load_dotenv()
+
+token = os.getenv('BOT_TOKEN')
+app_id = os.getenv('BOT_APPLICATION_ID')
 
 if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
@@ -301,4 +307,4 @@ async def load_cogs() -> None:
 
 asyncio.run(init_db())
 asyncio.run(load_cogs())
-bot.run(config["token"])
+bot.run(token)
